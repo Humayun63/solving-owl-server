f61bed13b972e7b3fb0d21c426c5f31e47b41af1
Humayun63 <humayun126224@gmail.com>
2023-08-05 23:42:06 +0600

    all routes created

--- index.js
+++ index.js
@@ -2,16 +2,85 @@ const express = require('express');
 const app = express()
 require('dotenv').config()
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req,res) =>{
+
+// mongoDB
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nucgrat.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        const problemsCollection = client.db('solvingOwl').collection('problems')
+
+        // All Problems
+        app.get('/all-problems', async (req, res) => {
+            const result = await problemsCollection.find({}, {projection: { title: 1, _id: 1 }}).toArray()
+            res.send(result)
+        })
+
+        // Easy Problems 
+        app.get('/easy-problems', async (req, res) => {
+            const query = { level: "easy" }
+            const result = await problemsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        // Medium Problems 
+        app.get('/medium-problems', async (req, res) => {
+            const query = { level: "medium" }
+            const result = await problemsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        // Advance Problems 
+        app.get('/advance-problems', async (req, res) => {
+            const query = { level: "advance" }
+            const result = await problemsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        // Single Problem
+        app.get('/problem/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await problemsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('Owl is solving the problems!')
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Owl is solving at ${port}`)
 })
